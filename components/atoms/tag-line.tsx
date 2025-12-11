2c04ac324cea4f33a6c125b21a6097541f34ad23
Jaredd11 <jaredd.tre.martin@gmail.com>
2025-12-11 12:29:06 +0530

    Refactor button, generating, heading, and tag-line components for improved readability and consistency by standardizing import order, formatting, and destructuring props.

--- components/atoms/tag-line.tsx
+++ components/atoms/tag-line.tsx
@@ -1,17 +1,19 @@
+import type React from "react";
 import { cn } from "@/lib/utils";
-import React from "react";
 import brackets from "../svg/brackets";
 
-type Props = { className?: string; children: React.ReactNode };
-
-const TagLine = ({ className, children }: Props) => {
-  return (
-    <div className={cn("tagline flex items-center", className)}>
-      {brackets("left")}
-      <div className="mx-3 text-n-3">{children}</div>
-      {brackets("right")}
-    </div>
-  );
-};
+const TagLine = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => (
+  <div className={cn("tagline flex items-center", className)}>
+    {brackets("left")}
+    <div className="mx-3 text-n-3">{children}</div>
+    {brackets("right")}
+  </div>
+);
 
 export default TagLine;
