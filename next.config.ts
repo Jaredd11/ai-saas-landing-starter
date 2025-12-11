c1004fd76ec87773a2568493fd7e18c672de15ed
Jaredd11 <jaredd.tre.martin@gmail.com>
2025-12-11 11:02:50 +0530

    chore(deps): update next to version 16.0.8 and refactor next.config.ts for improved readability

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,22 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizeCss: true,
-    reactCompiler: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        hostname: "raw.githubusercontent.com",
-      },
-      {
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-}
-export default nextConfig
+	reactCompiler: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	experimental: {
+		optimizeCss: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				hostname: "raw.githubusercontent.com",
+			},
+			{
+				hostname: "images.unsplash.com",
+			},
+		],
+	},
+};
+export default nextConfig;
